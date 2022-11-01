215da0b302a5c41f1c7ed6149863da29f7f481ba
Kassandra <K1995Moreno@gmail.com>
2022-10-31 22:58:54 -0400

    commit tree 8bff191fd722ef771bf6229c49592fd887b842a4

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// Multiply
+let num1= 31
+let num2= 2
+let multiply= num1*num2
+console.log(multiply)
 
-// Write your code here
+//Random
+let random= Math.floor(Math.random()*10)+1
+
+//Mod
+let num3= 24
+let num4= 5
+let mod= num3 % num4
+
+//Max
+let max= Math.max(16,18,19,3,4,20)
+
